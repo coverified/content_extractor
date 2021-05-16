8e6bdd3da1777cff2e39e194aa3ac94e35533d07
Kittl, Chris <chris.kittl@tu-dortmund.de>
2021-05-16 17:11:01 +0200

    Moving extraction logic to Extractor

--- src/main/scala/info/coverified/extractor/Extractor.scala
+++ src/main/scala/info/coverified/extractor/Extractor.scala
@@ -7,19 +7,25 @@ package info.coverified.extractor
 
 import com.typesafe.config.ConfigFactory
 import info.coverified.extractor.analyzer.Analyzer
+import info.coverified.extractor.config.Config
 import info.coverified.extractor.profile.ProfileConfig
 import info.coverified.graphql.Connector
+import info.coverified.graphql.schema.CoVerifiedClientSchema.Url.UrlView
 import info.coverified.graphql.schema.CoVerifiedClientSchema.{
+  CloudinaryImage_File,
   Entry,
   GeoLocation,
+  Language,
   LocationGoogle,
   Query,
   Source,
-  Url
+  Tag,
+  Url,
+  _QueryMeta
 }
 import sttp.client3.asynchttpclient.zio.{SttpClient, send}
 import sttp.model.Uri
-import zio.ZIO
+import zio.{RIO, ZIO}
 import zio.console.Console
 
 import java.io.File
@@ -30,28 +36,54 @@ import java.io.File
   * @version 0.1
   * @since 26.02.21
   */
-final case class Extractor(apiUrl: Uri) {
+final case class Extractor private (apiUrl: Uri, profileDirectoryPath: String) {
+  /* Defining types as shorthands */
+  type UrlView = Url.UrlView[Source.SourceView[
+    GeoLocation.GeoLocationView[LocationGoogle.LocationGoogleView]
+  ]]
+  type EntryView =
+    Entry.EntryView[CloudinaryImage_File.CloudinaryImage_FileView, Tag.TagView[
+      Language.LanguageView,
+      CloudinaryImage_File.CloudinaryImage_FileView
+    ], _QueryMeta._QueryMetaView, Language.LanguageView, Source.SourceView[
+      GeoLocation.GeoLocationView[LocationGoogle.LocationGoogleView]
+    ]]
 
-  def getExistingEntries: ZIO[Console with SttpClient, Throwable, List[_]] = ???
+  /**
+    * Build the extraction effect. It consists of the following steps:
+    * <ol>
+    *   <li>Acquire all known configs</li>
+    *   <li>Acquire all known urls</li>
+    *   <li>Find applicable config</li>
+    *   <li>Issue content extraction</li>
+    * </ol>
+    *
+    * @return
+    */
+  def buildExtractionEffect(): ZIO[Console with SttpClient, Throwable, Unit] = {
+    val urlToProfileConfigs = getAllConfigs(profileDirectoryPath)
 
-  def getAllUrls = {
-    // get existent urls
-    val urlsQuery = Query.allUrls()(
-      Url.view(
-        Source.view(
-          GeoLocation.view(LocationGoogle.view)
-        )
+    /* Getting hands on that ZIO stuff - flat mapping by for-comprehension */
+    for {
+      availableUrlViews <- getAllUrlViews
+      _ <- ZIO.collectAllPar(
+        availableUrlViews.flatMap(urlViewToEntryView(_, urlToProfileConfigs))
       )
-    )
-    val existingUrls = Connector
-      .sendRequest(urlsQuery.toRequest(apiUrl))
-      .map(_.map(_.flatten).getOrElse(List.empty))
-    existingUrls
+    } yield ()
   }
 
-  def getAllConfigs(cfgFolderPath: File): Map[String, ProfileConfig] = {
-    if (cfgFolderPath.exists() && cfgFolderPath.isDirectory) {
-      cfgFolderPath.listFiles
+  /**
+    * Acquire all page profiles available in the given directory path.
+    *
+    * @param cfgDirectoryPath The path, where the files are located
+    * @return A map from pages' url to their applicable config
+    */
+  private def getAllConfigs(
+      cfgDirectoryPath: String
+  ): Map[String, ProfileConfig] = {
+    val cfgDirectory = new File(cfgDirectoryPath)
+    if (cfgDirectory.exists() && cfgDirectory.isDirectory) {
+      cfgDirectory.listFiles
         .filter(_.isFile)
         .map(file => ProfileConfig(ConfigFactory.parseFile(file)))
         .map(profileCfg => profileCfg.profile.hostname -> profileCfg)
@@ -61,22 +93,85 @@ final case class Extractor(apiUrl: Uri) {
     }
   }
 
-  def getProfile4Url(
+  /**
+    * Asking the Connector for all available urls + additional information within the data source
+    *
+    * @return An effect, that evaluates to a list of [[UrlView]]s
+    */
+  private def getAllUrlViews
+      : ZIO[Console with SttpClient, Throwable, List[UrlView]] = {
+    val urlsQuery = Query.allUrls()(
+      Url.view(
+        Source.view(
+          GeoLocation.view(LocationGoogle.view)
+        )
+      )
+    )
+    Connector
+      .sendRequest(urlsQuery.toRequest(apiUrl))
+      .map(_.map(_.flatten).getOrElse(List.empty))
+  }
+
+  /**
+    * Based on the received url view, try to find matching profile and acquire and view onto the entry, that it forms
+    * together
+    *
+    * @param urlView              View onto the url
+    * @param urlToProfileConfigs  Mapping from url to profile config to use
+    * @return An effect, that evaluates to an [[Option]] of [[EntryView]]
+    */
+  private def urlViewToEntryView(
+      urlView: UrlView,
+      urlToProfileConfigs: Map[String, ProfileConfig]
+  ): Option[RIO[Console with SttpClient, Option[EntryView]]] = urlView match {
+    case UrlView(_, _, Some(url), Some(source)) =>
+      getProfile4Url(url, urlToProfileConfigs).flatMap(
+        profileCfg => getMutations(url, source.id, profileCfg)
+      )
+    case _ => None
+  }
+
+  /**
+    * Given a map of hostnames to configs, find that entry, whose hostname is included within the queried url.
+    *
+    * @param url              Queried url
+    * @param hostNameToConfig Mapping a host name onto it's applicable config
+    * @return An [[Option]] onto an applicable config
+    */
+  private def getProfile4Url(
       url: String,
-      configs: Map[String, ProfileConfig]
-  ): Option[ProfileConfig] = {
-    configs
+      hostNameToConfig: Map[String, ProfileConfig]
+  ): Option[ProfileConfig] =
+    hostNameToConfig
       .find {
-        case (hostname, cfg) =>
+        case (hostname, _) =>
           url.contains(hostname)
       }
       .map(_._2)
-  }
 
-  def getMutations(url: String, sourceId: String, cfg: ProfileConfig) = {
+  /**
+    * Issue the analyser and try to mutate the given url together with the config into an entry
+    *
+    * @param url      Queried url
+    * @param sourceId The id of the source
+    * @param cfg      [[ProfileConfig]] to use
+    * @return An [[Option]] onto an effect, that evaluates to an [[Option]] onto an [[EntryView]]
+    */
+  def getMutations(
+      url: String,
+      sourceId: String,
+      cfg: ProfileConfig
+  ): Option[RIO[Console with SttpClient, Option[EntryView]]] = {
     Analyzer
       .run(url, sourceId, cfg)
       .flatMap(_.map(mut => Connector.sendRequest(mut.toRequest(apiUrl))))
   }
 
+  def getExistingEntries: ZIO[Console with SttpClient, Throwable, List[_]] = ???
+
+}
+
+object Extractor {
+  def apply(config: Config): Extractor =
+    new Extractor(config.apiUri, config.profileDirectoryPath)
 }
--- src/main/scala/info/coverified/extractor/Run.scala
+++ src/main/scala/info/coverified/extractor/Run.scala
@@ -6,13 +6,10 @@
 package info.coverified.extractor
 
 import com.typesafe.scalalogging.LazyLogging
-import info.coverified.extractor.ArgsParser.Args
 import info.coverified.extractor.config.Config
-import sttp.client3.UriContext
 import sttp.client3.asynchttpclient.zio.AsyncHttpClientZioBackend
 import zio.{App, ExitCode, URIO, ZIO}
 
-import java.io.File
 import scala.util.{Failure, Success}
 
 /**
@@ -25,51 +22,28 @@ object Run extends App with LazyLogging {
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
 
-    // get args
-    val (extractor, configFolderPath): (Extractor, String) = {
-      val config = ArgsParser
-        .parse(args.toArray)
-        .flatMap(Config.fromArgs(_).toOption) match {
-        case Some(config) => config
-        case None =>
-          logger.info(
-            "Trying to get configuration from environment variables ... "
-          )
-          Config.fromEnv() match {
-            case Success(config) => config
-            case Failure(exception) =>
-              throw new RuntimeException(
-                "Config parameters missing!",
-                exception
-              )
-          }
-      }
-      (Extractor(uri"${config.apiUri}"), config.profileDirectoryPath)
+    /* Try to gather config from CLI args or environment variables */
+    val config = ArgsParser
+      .parse(args.toArray)
+      .flatMap(Config.fromArgs(_).toOption) match {
+      case Some(config) => config
+      case None =>
+        logger.info(
+          "Trying to get configuration from environment variables ... "
+        )
+        Config.fromEnv() match {
+          case Success(config) => config
+          case Failure(exception) =>
+            throw new RuntimeException(
+              "Config parameters missing!",
+              exception
+            )
+        }
     }
 
-    // debug
-    val configs = extractor.getAllConfigs(new File(configFolderPath))
-
-    val extractorRun = for {
-      urls <- extractor.getAllUrls
-      _ <- ZIO.collectAll(
-        urls.flatMap(
-          urlView =>
-            urlView.url.zip(urlView.source).flatMap {
-              case (urlString, source) =>
-                extractor
-                  .getProfile4Url(urlString, configs)
-                  .flatMap(
-                    profileCfg =>
-                      extractor.getMutations(urlString, source.id, profileCfg)
-                  )
-            }
-        )
-      )
-    } yield ()
-
-    extractorRun.provideCustomLayer(AsyncHttpClientZioBackend.layer()).exitCode
-
+    Extractor(config)
+      .buildExtractionEffect()
+      .provideCustomLayer(AsyncHttpClientZioBackend.layer())
+      .exitCode
   }
-
 }
