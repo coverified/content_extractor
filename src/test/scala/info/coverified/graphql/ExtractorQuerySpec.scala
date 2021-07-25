41ed90aca90e6a1adf30c8a39dbc16f30996fbf0
Kittl, Chris <chris.kittl@tu-dortmund.de>
2021-07-25 20:55:21 +0200

    Fix broken test

--- src/test/scala/info/coverified/graphql/ExtractorQuerySpec.scala
+++ src/test/scala/info/coverified/graphql/ExtractorQuerySpec.scala
@@ -33,7 +33,7 @@ class ExtractorQuerySpec extends MockServerSpec with GraphQlHelper {
             |        "url": {
             |          "id": "ckr7fdbuw0218fdo803zdc1hy"
             |        },
-            |        "tags": [],
+            |        "articleTags": [],
             |        "language": null,
             |        "content": "",
             |        "summary": "",
@@ -59,7 +59,7 @@ class ExtractorQuerySpec extends MockServerSpec with GraphQlHelper {
           mockServer.verify(
             postRequestedFor(urlEqualTo("/api/graphql")).withRequestBody(
               new EqualToPattern(
-                """{"query":"query{allEntries(where:{url:{id:\"ckr7fdbuw0218fdo803zdc1hy\"}},orderBy:[],skip:0){id name content summary url{id} date disabled tags(where:{},orderBy:[],skip:0){id name language{id} highlighted generated}}}","variables":{}}"""
+                """{"query":"query{allEntries(where:{url:{id:\"ckr7fdbuw0218fdo803zdc1hy\"}},orderBy:[],skip:0){id name content summary url{id} date disabled articleTags(where:{},orderBy:[],skip:0){id name}}}","variables":{}}"""
               )
             )
           )
